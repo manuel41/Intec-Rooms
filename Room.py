e0c5cdc7ea646ee69d3408e9af86e515801337ea
manuel41 <medmundogonzalez@gmail.com>
2019-11-26 00:42:15 -0400

    Create Room Class

---
+++ Room.py
@@ -0,0 +1,8 @@
+class Room(object):
+    def __init__(self, monday, tuesday, wednesday, thursday, friday, saturday):
+        self.monday = []
+        self.tuesday = []
+        self.wednesday = []
+        self.thursday = []
+        self.friday = []
+        self.saturday = []
