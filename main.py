233252fc16d0bedc2e5458b6b6502a3873ef4b54
manuel41 <medmundogonzalez@gmail.com>
2019-11-26 15:37:59 -0400

    Add user ID and password input

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from bs4 import BeautifulSoup as soup
 import urllib.request as uReq
 
+login = input('Inserte su ID: ')
+passw = input('Inserte su contraseña: ')
+
+login_data = {
+    'txtID' : login
+    'txtUserPass' : passw
+}
+
 import requests
 with requests.Session() as s:
     url = "https://procesos.intec.edu.do/"
