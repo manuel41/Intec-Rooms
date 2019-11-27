d75d224fc1cf8e9f82d94a9de1141fa609e20541
Manuel Gonzalez <49133037+manuel41@users.noreply.github.com>
2019-11-27 17:39:09 -0400

    In development

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 from bs4 import BeautifulSoup as soup
 import urllib.request as uReq
+from Room import Room
 
-login = input('Inserte su ID: ')
-passw = input('Inserte su contraseña: ')
+#login = input('Inserte su ID: ')
+#passw = input('Inserte su contraseña: ')
 
-login_data = {
-    'txtID' : login,
-    'txtUserPass' : passw
-}
+# login_data = {
+#     'txtID' : login,
+#     'txtUserPass' : passw
+# }
 
-import requests
-with requests.Session() as s:
-    url = "https://procesos.intec.edu.do/"
-    r = s.get(url)
-    pos_req = s.post(url, data=login_data)
-    print(pos_req.content)
+# import requests
+# with requests.Session() as s:
+#     url = "https://procesos.intec.edu.do/"
+#     r = s.get(url)
+#     pos_req = s.post(url, data=login_data)
+#     print(pos_req.content)
+
+rooms = []
+with open('Oferta.html') as oferta_get:
+    source = soup(oferta_get, 'lxml')
+    asig_tags = source.findAll('td', class_='uk-text-center small-column')
+    tag = 0
+    for x in asig_tags:
+        if tag == 0:
+            newroom = Room()
+        if tag == 3:
+            for y in rooms:
+                if rooms[y].name == x.text:
+                    pass
+            newroom.name = x.text
+        if tag == 4
+
+        #tag+=1
